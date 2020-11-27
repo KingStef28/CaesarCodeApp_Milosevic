f2ea9192127c343812f559b4964ff1330bce8db7
Stefan Milosevic <smilosevic86@outlook.it>
2020-11-27 15:17:55 +0100

    Completamento scrittura main e test di funzionamento

--- CaesarCodeApp_Milosevic/Program.cs
+++ CaesarCodeApp_Milosevic/Program.cs
@@ -7,7 +7,27 @@ namespace CaesarCodeApp_Milosevic
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci una parola:");
+            string plaintext = Console.ReadLine();
+            int key;
+            CaesarCode m1;
 
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Inserisci la chiave:");
+                    key = int.Parse(Console.ReadLine());
+                } while (key < 0 || key > 25);
+
+                m1 = new CaesarCode(plaintext, key);
+
+                Console.WriteLine($"Il testo cifrato è: {m1.CipherText}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
