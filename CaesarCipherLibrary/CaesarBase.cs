805e9ea9c74e4a082f3323d7e2397888d7fbd573
Stefan Milosevic <smilosevic86@outlook.it>
2020-11-27 15:13:28 +0100

    Completamento scrittura codice librerie

--- CaesarCipherLibrary/CaesarBase.cs
+++ CaesarCipherLibrary/CaesarBase.cs
@@ -2,7 +2,37 @@
 
 namespace CaesarCipherLibrary
 {
-    public class CaesarBase
+    public abstract class CaesarBase
     {
+        protected int Key { get; set; }
+
+        private char Cipher(char ch)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset;
+
+            if (char.IsUpper(ch))
+            {
+                offset = 'A';
+            }
+            else
+            {
+                offset = 'a';
+            }
+
+            return (char)(((ch - offset + Key) % 26) + offset);
+        }
+
+        protected string DoWork(string m)
+        {
+            string message = string.Empty;
+
+            foreach (char ch in m)
+                message += Cipher(ch);
+
+            return message;
+        }
     }
 }
--- CaesarCodeApp_Milosevic/Program.cs
+++ CaesarCodeApp_Milosevic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CaesarCipherLibrary;
 
 namespace CaesarCodeApp_Milosevic
 {
